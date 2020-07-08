807d973524958a4881440114b5950ecaf0d22eb0
stevemeister5 <stephenreubenm@gmail.com>
2020-07-08 13:11:10 +0300

    Working Multiple Notes

--- MarkDown/script.js
+++ MarkDown/script.js
@@ -6,7 +6,13 @@ new Vue({
     // Some Data
     data () {
         return {
-            content: 'This is a note.',
+
+            content: localStorage.getItem('content') || 'You can write in **markdown**',
+            notes: [],
+            
+            // ID of the selected note
+            selectedId: null,
+            
         }
     },
 
@@ -14,29 +20,59 @@ new Vue({
     computed: {
         notePreview () {
             // Markdown rendered to HTML
-            return marked(this.content)
+            return this.selectedNote ? marked(this.selectedNote.content) : ''
         },
+
+        addButtonTitle () {
+            return this.notes.length + ' note(s) already'
+        },
+
+        selectedNote() {
+            // Return the matching note with selectedId
+            return this.notes.find(note => note.id === this.selectedId)
+        },
+
     },
 
     // change watchers 
-    watch: {
-        // watching content data property
-        content: {
-            handler: 'saveNote',
-        },
-    },
+    // watch: {
+    //     // watching content data property
+    //     content: {
+    //         handler: 'saveNote',
+    //     },
+    // },
 
     // Called when the instance is ready 
-    created() {
-        // Set the content to the stored value
-        this.content = localStorage.getItem('content') || 'You can write in **markdown**'
-    },
+    // created() {
+    //     // Set the content to the stored value
+    //     this.content = localStorage.getItem('content') || 'You can write in **markdown**'
+    // },
 
     // Methods 
     methods: {
-        saveNote (val) {
-            console.group('saving note:', val)
-            localStorage.setItem('content', val)
+        // saveNote (val) {
+        //     console.group('saving note:', val)
+        //     localStorage.setItem('content', val)
+        // },
+
+        // Add a note with some default content and select it
+        addNote () {
+            const time = Date.now()
+            // Default new note
+            const note = {
+                id: String(time),
+                title: 'New note ' + (this.notes.length + 1),
+                content: '**Hi!** This noteboook is using [markdown] for formattign!',
+                created: time,
+                favorite: false,
+            }
+
+            // add to the list 
+            this.notes.push(note)
+        },
+
+        selectNote (note) {
+            this.selectedId = note.id
         },
     },
 
