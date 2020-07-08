04c53beed7f4ba5650dc10ccc6b7382c66a53a5b
stevemeister5 <stephenreubenm@gmail.com>
2020-07-08 14:37:14 +0300

    State setup and linking

--- CastleDuel/main.js
+++ CastleDuel/main.js
@@ -1,4 +1,20 @@
 new Vue({
     name: 'game',
     el: '#app',
+
+    data: state,
+
+    template: `<div id="#app">
+        {{ worldRatio }}
+    </div>`,
+
+    mounted () {
+        console.log(this.$data === state)
+    },
+    
+})
+
+// Window resize handling
+window.addEventListener('resize', () => {
+    state.worldRatio = getWorldRatio()
 })
