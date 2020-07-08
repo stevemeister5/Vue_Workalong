d95ed9aaa031e479fe0b4dd3419b9cd450bd530e
stevemeister5 <stephenreubenm@gmail.com>
2020-07-08 13:56:47 +0300

    Complete Rename, delete, favorite bar

--- MarkDown/script.js
+++ MarkDown/script.js
@@ -31,6 +31,12 @@ new Vue({
             return this.notes.find(note => note.id === this.selectedId)
         },
 
+        sortedNotes () {
+            return this.notes.slice()
+                .sort((a, b) => a.created - b.created)
+                .sort((a, b) => (a.favorite === b.favorite) ? 0 : a.favorite? -1 : 1)
+        },
+
     },
 
     // change watchers 
